db15228bea09fc2226f753decf9f8fc6dd792885
Sam Woodman <swoodman213@gmail.com>
2020-04-17 09:05:48 -0700

    Added commented code for running functions

--- tradeoffs/Management scenarios/Funcs_management_scenarios.R
+++ tradeoffs/Management scenarios/Funcs_management_scenarios.R
@@ -1,5 +1,31 @@
-# TODO: func2: Calculate risk and aggregate by month
+###############################################################################
+# Script for running effort_mgmt() function (currently set to Sam's file paths)
 
+# library(dplyr)
+# library(lubridate)
+# 
+# x.hump <- readRDS("../raimbow-local/Outputs/Humpback_5km_long_monthly.rds") %>% 
+#   mutate(year_month = paste(year(date), sprintf("%02d", month(date)), sep = "_")) %>% 
+#   select(GRID5KM_ID, year_month, Humpback_dens_mean, Humpback_dens_se)
+# 
+# x.orig <- readRDS("../raimbow-local/Data/fishing/CA_DCRB_vms_fishing_daily_2009-2018_fishtix_humpback_blue_whales_grids.RDS") %>% 
+#   select(-year_mo, -contains("risk"), -contains("H_Avg_Abund")) %>% 
+#   left_join(x.hump, by = c("year_month", "GRID5KM_ID"))
+# rm(x.hump)
+# 
+# 
+# # source("tradeoffs/Management scenarios/Funcs_management_scenarios.R")
+# d <- effort_mgmt(
+#   x = x.orig, 
+#   delay.date = as.Date("2009-12-15"), 
+#   delay.region = "CenCA", 
+#   delay.method.shift = "lag", 
+#   delay.method.fidelity = "temporal", 
+#   closure.date = as.Date("2010-04-01), 
+#   closure.region = "CenCA", 
+#   closure.method = "temporal", 
+#   closure.redist.percent = 10
+# )
 
 
 ###############################################################################
