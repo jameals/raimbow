dc0af72e849204e52ed39d25645706a19706d46f
jameals <jameal.samhouri@noaa.gov>
2020-06-02 16:10:33 -0700

    added file paths for JS, updated to new keys

--- tradeoffs/join_fishing_data_regions_depth.R
+++ tradeoffs/join_fishing_data_regions_depth.R
@@ -6,15 +6,12 @@ con_df_daily_years_5km_CA_no_regions <- readRDS("~/Documents/RAIMBOW/Processed D
   select(-contains("risk"), -contains("H_Avg_Abund"), -contains("Blue_"), 
          -Region, -CA_OFFSHOR, -BAND_25KM , -BAND_50KM ) #, -BIA_mn_noNAs, -BIA_bm_noNAs, -BIA_bm_or_mn)
 
-grid.key <- readRDS("/Users/jameal.samhouri/Documents/RAIMBOW/Processed Data/5x5 Grid/Grid5km_key_region.rds") %>% 
+grid.key <- readRDS("/Users/jameal.samhouri/Documents/RAIMBOW/Processed Data/5x5 Grid/Grid5km_key.rds") %>% 
   select(-region_ts)
-
-grid.depth <- readRDS("/Users/jameal.samhouri/Documents/RAIMBOW/Processed Data/5x5 Grid/Grid5km_depth.rds")
 
 con_df_daily_years_5km_CA_all_vessels_regions_depths <- con_df_daily_years_5km_CA_no_regions %>% 
   left_join(grid.key, by = "GRID5KM_ID") %>% 
-  left_join(grid.depth, by = "GRID5KM_ID") %>%
-  mutate(Region = ifelse(Region == "OR", "NorCA", Region)) #TODO: discuss these/update effort_mgmt to handle other regions
+  mutate(Region = ifelse(Region == "OR", "NorCA", Region)) #TODO: discuss these/update effort_mgmt to handle other regions. could choose just to exclude grid cells with centroids in Oregon
 stopifnot(nrow(grid.key) == nrow(distinct(select(con_df_daily_years_5km_CA_all_vessels_regions_depths, GRID5KM_ID, Region, CA_OFFSHOR))))
 
 write_rds(con_df_daily_years_5km_CA_all_vessels_regions_depths, "~/Documents/RAIMBOW/Processed Data/VMS/CA_DCRB_vms_fishing_daily_2009-2019_all_vessels_regions_depths.RDS")
@@ -24,14 +21,11 @@ con_df_daily_years_5km_CA_no_regions_sm <- readRDS("~/Documents/RAIMBOW/Processe
   select(-contains("risk"), -contains("H_Avg_Abund"), -contains("Blue_"), 
          -Region, -CA_OFFSHOR, -BAND_25KM , -BAND_50KM ) #, -BIA_mn_noNAs, -BIA_bm_noNAs, -BIA_bm_or_mn)
 
-grid.key <- readRDS("/Users/jameal.samhouri/Documents/RAIMBOW/Processed Data/5x5 Grid/Grid5km_key_region.rds") %>% 
+grid.key <- readRDS("/Users/jameal.samhouri/Documents/RAIMBOW/Processed Data/5x5 Grid/Grid5km_key.rds") %>% 
   select(-region_ts)
-
-grid.depth <- readRDS("/Users/jameal.samhouri/Documents/RAIMBOW/Processed Data/5x5 Grid/Grid5km_depth.rds")
 
 con_df_daily_years_5km_CA_small_vessels_regions_depths <- con_df_daily_years_5km_CA_no_regions_sm %>% 
   left_join(grid.key, by = "GRID5KM_ID") %>% 
-  left_join(grid.depth, by = "GRID5KM_ID") %>%
   mutate(Region = ifelse(Region == "OR", "NorCA", Region)) #TODO: discuss these/update effort_mgmt to handle other regions
 stopifnot(nrow(grid.key) >= nrow(distinct(select(con_df_daily_years_5km_CA_small_vessels_regions_depths, GRID5KM_ID, Region, CA_OFFSHOR)))) # changed to greater than or equal to because the number of grid cells small vessels operate in may be fewer than the number of grid cells all vessels operate in
 
@@ -42,14 +36,11 @@ con_df_daily_years_5km_CA_no_regions_lg <- readRDS("~/Documents/RAIMBOW/Processe
   select(-contains("risk"), -contains("H_Avg_Abund"), -contains("Blue_"), 
          -Region, -CA_OFFSHOR, -BAND_25KM , -BAND_50KM ) #, -BIA_mn_noNAs, -BIA_bm_noNAs, -BIA_bm_or_mn)
 
-grid.key <- readRDS("/Users/jameal.samhouri/Documents/RAIMBOW/Processed Data/5x5 Grid/Grid5km_key_region.rds") %>% 
+grid.key <- readRDS("/Users/jameal.samhouri/Documents/RAIMBOW/Processed Data/5x5 Grid/Grid5km_key.rds") %>% 
   select(-region_ts)
-
-grid.depth <- readRDS("/Users/jameal.samhouri/Documents/RAIMBOW/Processed Data/5x5 Grid/Grid5km_depth.rds")
 
 con_df_daily_years_5km_CA_large_vessels_regions_depths <- con_df_daily_years_5km_CA_no_regions_lg %>% 
   left_join(grid.key, by = "GRID5KM_ID") %>% 
-  left_join(grid.depth, by = "GRID5KM_ID") %>%
   mutate(Region = ifelse(Region == "OR", "NorCA", Region)) #TODO: discuss these/update effort_mgmt to handle other regions
 stopifnot(nrow(grid.key) >= nrow(distinct(select(con_df_daily_years_5km_CA_large_vessels_regions_depths, GRID5KM_ID, Region, CA_OFFSHOR)))) # changed to greater than or equal to because the number of grid cells large vessels operate in may be fewer than the number of grid cells all vessels operate in
 
