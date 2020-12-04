40bd297437c488c409e817898c61ccb5d3926186
Sam Woodman <swoodman213@gmail.com>
2020-12-04 11:06:39 -0800

    Sam making some plots

--- humpback_risk/plot_raimbow.R
+++ humpback_risk/plot_raimbow.R
@@ -16,7 +16,7 @@ plot_raimbow <- function(x, x.col, grid.sf = NULL, map.base = NULL,
 
 
 #------------------------------------------------------------------------------
-plot_raimbow.data.frame <- function(x, x.col, grid.sf, map.base, map.b1, map.b2, 
+plot_raimbow.data.frame <- function(x, x.col, grid.sf, map.base, map.i050, map.i100, map.i200, 
                                     col.breaks, col.pal, 
                                     key.pos, reset, ...) {
   if (is.null(grid.sf)) stop("grid.sf cannot be NULL if x is a data frame")
@@ -29,13 +29,13 @@ plot_raimbow.data.frame <- function(x, x.col, grid.sf, map.base, map.b1, map.b2,
     left_join(grid.sf, by = "GRID5KM_ID") %>% 
     st_sf(agr = "constant")
   
-  plot_raimbow(tmp, x.col, NULL, map.base, map.b1, map.b2, col.breaks, col.pal, 
+  plot_raimbow(tmp, x.col, NULL, map.base, map.i050, map.i100, map.i200, col.breaks, col.pal, 
                key.pos, reset, ...)
 }
 
 
 #------------------------------------------------------------------------------
-plot_raimbow.sf <- function(x, x.col, grid.sf, map.base, map.b1, map.b2, 
+plot_raimbow.sf <- function(x, x.col, grid.sf, map.base, map.i050, map.i100, map.i200, 
                             col.breaks, col.pal, key.pos, reset, ...) {
   stopifnot(require(maps))
   if (is.null(map.base))
@@ -48,8 +48,13 @@ plot_raimbow.sf <- function(x, x.col, grid.sf, map.base, map.b1, map.b2,
        key.pos = NULL, reset = FALSE, 
        ...)
   plot(map.base, add = TRUE, col = "tan", border = NA)
-  if (!is.null(map.b1)) plot(map.b1, add = TRUE, col = "grey", lwd = 0.5)
-  if (!is.null(map.b2)) plot(map.b2, add = TRUE, col = "black", lwd = 0.5)
+  if (!is.null(map.i050)) plot(map.i050, add = TRUE, col = "red", lwd = 0.5)
+  if (!is.null(map.i100)) plot(map.i100, add = TRUE, col = "grey", lwd = 0.5)
+  if (!is.null(map.i200)) plot(map.i200, add = TRUE, col = "black", lwd = 0.5)
+  
+  abline(h = 42, col = "black")
+  abline(h = 38.833, col = "black")
+  abline(h = 34.5, col = "black")
   graphics::box()
   
   if ("xaxt" %in% names(lst)) if (lst$xaxt == "n") .degAxis(1, at = c(-125, -120))
@@ -88,8 +93,9 @@ legend_raimbow <- function(col.breaks, sprintf.code = "%0.2f",
 # Default legend for contour lines
 legend.raimbow.bathy <- function(...) {
   legend(
-    "right", title = "Isobaths", legend = c("100m", "200m"), 
-    col = c("grey", "black"), pch = NA, lty = 1, lwd = 2, cex = 1, 
+    "right", title = "Isobaths", legend = c("50m", "100m", "200m"), 
+    col = c("red", "grey", "black"), 
+    pch = NA, lty = 1, lwd = 2, cex = 1, 
     ...
   )
 }
