4ee438af8ad42acc10643521869b3fc068367e56
Leena <leena.riekkola@noaa.gov>
2021-06-22 09:42:11 +1200

    tidy code

--- wdfw/logbook_mapping_fxns__script8_plotting trap densities.R
+++ wdfw/logbook_mapping_fxns__script8_plotting trap densities.R
@@ -32,6 +32,8 @@ theme_set(plot_theme)
 options(dplyr.summarise.inform = FALSE)
 
 
+#----------------------------------
+
 ## Cleaned and summarized, simulated crab trap data
 adj_summtraps <- read_rds(here::here('wdfw','data','adj_summtraps.rds'))
 
@@ -45,10 +47,11 @@ adj_summtraps <- adj_summtraps %>%
 adj_summtraps <- adj_summtraps %>% 
   filter(!GRID5KM_ID %in% c(117310, 117311, 117640, 120280, 120610, 120940, 122258, 122259, 122588))
 
-#create a column in df to indicate whether data are winter or summer 
-#summer is data after May 1 
-#the 'periods' included in summer are:
-#May_1, May-2, June_1, June_2, July_1, July_2, AUgust_1, August_2, September_1, September_2
+
+# create a column in df to indicate whether data are 'winter' or 'summer' 
+# summer is data after May 1 
+# the 'periods' included in summer are:
+# May_1, May-2, June_1, June_2, July_1, July_2, August_1, August_2, September_1, September_2
 adj_summtraps_wintersummer <- adj_summtraps %>% 
   mutate(wintersummer = 
            ifelse(month_interval %in% c('May_1', 'May_2', 'June_1', 'June_2', 'July_1', 'July_2', 'August_1', 'August_2', 'September_1','September_2')
@@ -57,51 +60,25 @@ adj_summtraps_wintersummer <- adj_summtraps %>%
 
 
 
-#simple histogram 
+# bar chart of proportions -- all data
 p1 <- adj_summtraps_wintersummer %>% 
-  ggplot(aes(M1_trapdens, color=wintersummer, fill=wintersummer)) +
-    geom_histogram() +
-  ggtitle('Histogram of M1 trap densities in winter/summer')
+  ggplot(aes(color=wintersummer, fill=wintersummer)) +
+  geom_bar(aes(x=M1_trapdens, y=stat(prop)), position = "dodge") +
+  scale_x_binned() + 
+  ggtitle('Bar chart of proportions - M1 trap densities in winter/summer')
 p1
 
 p2 <- adj_summtraps_wintersummer %>% 
-  ggplot(aes(M2_trapdens, color=wintersummer, fill=wintersummer)) +
-  geom_histogram() +
-  ggtitle('Histogram of M2 trap densities in winter/summer')
-p2
-
-
-# bar chart of proportions
-#all data
-p3 <- adj_summtraps_wintersummer %>% 
-  ggplot(aes(color=wintersummer, fill=wintersummer)) +
-  geom_bar(aes(x=M1_trapdens, y=stat(prop)), position = "dodge") +
-  scale_x_binned(breaks=seq(0, 85, 5)) + #n.breaks = 15
-  ggtitle('Bar chart of proportions - M1 trap densities in winter/summer')
-p3
-
-p4 <- adj_summtraps_wintersummer %>% 
   ggplot(aes(color=wintersummer, fill=wintersummer)) +
   geom_bar(aes(x=M2_trapdens, y=stat(prop)), position = "dodge") +
-  scale_x_binned(breaks=seq(0, 145, 5)) +
+  scale_x_binned() + #you can specify x-axis break here, e.g.: breaks=seq(0, 125, 5)
   ggtitle('Bar chart of proportions - M2 trap densities in winter/summer')
-p4
+p2
 
-map_out <- plot_grid(p3,p4,nrow=1)
-ggsave(here('wdfw','plots',paste0('Plot of trap densitites_alldblgridsremoved','.png')),map_out,w=12,h=10)
+map_out <- plot_grid(p1,p2,nrow=1)
+ggsave(here('wdfw','plots',paste0('Plot of trap densitites_by season','.png')),map_out,w=12,h=10)
 
-#frequency polygon 
-p5 <- adj_summtraps_wintersummer %>% 
-  ggplot(aes(M1_trapdens, after_stat(density), color=wintersummer, fill=wintersummer)) +
-  geom_freqpoly(binwidth = 0.5) + #binwidth = 5 
-  ggtitle('Frequency polygon - M1 trap densities in winter/summer')
-p5
 
-p6 <- adj_summtraps_wintersummer %>% 
-  ggplot(aes(M2_trapdens, after_stat(density), color=wintersummer, fill=wintersummer)) +
-  geom_freqpoly(binwidth = 0.5) + #binwidth = 5 
-  ggtitle('Frequency polygon - M2 trap densities in winter/summer')
-p6
 
 
 ################
