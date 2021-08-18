ee91ea56587c4160c4a6fecd898539154733b9f6
Leena <leena.riekkola@noaa.gov>
2021-08-19 07:43:48 +1200

    testing script 6 mapping on OR data

--- wdfw/code/OR/OR_logbook_analysis_script6_mapping.R
+++ wdfw/code/OR/OR_logbook_analysis_script6_mapping.R
@@ -41,7 +41,7 @@ options(dplyr.summarise.inform = FALSE)
 # Cleaned and summarized, simulated crab trap data
 adj_summtraps <- read_rds(here::here('wdfw','data', 'OR','OR_adj_summtraps.rds'))
 #Note that there were few really high trap dens values for grids that are odd shaped and close to shore
-# Read in data filtered for SpatialFlag
+# Alternatively, read in version of data filtered for SpatialFlag (fixes the issue of very high values)
 #adj_summtraps <- read_rds(here::here('wdfw', 'data', 'OR', 'OR_adj_summtraps_SpatialFlag_filtered.rds'))
 
 # Read in spatial grid data 
@@ -118,7 +118,7 @@ conf_dat2 <-  conf_dat %>%
 
 # Figure out good trap density scale
 #Note that there were few really high trap dens values for grids that are odd shaped and close to shore
-#excluding those highest value was 107
+#excluding those, highest values are 107, 95, 83, the rest are below 80
 adj_summtraps %>% 
   ggplot()+
   geom_density(aes(M2_trapdens))
@@ -136,8 +136,7 @@ map_traps <- function(gridded_traps,saveplot=TRUE){
   t1 <- paste0(season_label,"\n",month_label,", ",period_label, " Method 1")
   t2 <- paste0(season_label,"\n",month_label,", ",period_label, " Method 2")
 
-  #will need to adjust bbox from WA to OR
-  bbox = c(800000,1650000,1013103,1970000)
+  bbox = c(800000,1170000,1025000,1920000) #adjusted for OR data
 
   # include this if want comparison maps of M1 and M2 methods  
   # M1_map_out <- gridded_traps %>% 
@@ -154,9 +153,9 @@ map_traps <- function(gridded_traps,saveplot=TRUE){
     ggplot()+
     geom_tile(aes(grd_x,grd_y,fill=M2_trapdens),na.rm=T,alpha=0.8)+
     geom_sf(data=coaststates,col=NA,fill='gray50')+
-    geom_sf(data=MA_shp,col="black", size=0.5, fill=NA)+
-    geom_sf(data=QSMA_shp,col="black", linetype = "11", size=0.5, fill=NA)+
-    scale_fill_viridis(na.value='grey70',option="C",limits=c(0,120),breaks=c(0,30,60,90,120),oob=squish)+
+    #geom_sf(data=MA_shp,col="black", size=0.5, fill=NA)+
+    #geom_sf(data=QSMA_shp,col="black", linetype = "11", size=0.5, fill=NA)+
+    scale_fill_viridis(na.value='grey70',option="C",limits=c(0,80),breaks=c(0,20,40,60,80),oob=squish)+
     coord_sf(xlim=c(bbox[1],bbox[3]),ylim=c(bbox[2],bbox[4]))+
     #if you do NOT want to show lat/lon lines on the map, use the below line instead:
     #coord_sf(xlim=c(bbox[1],bbox[3]),ylim=c(bbox[2],bbox[4]),datum=NA)+
@@ -169,7 +168,7 @@ map_traps <- function(gridded_traps,saveplot=TRUE){
   # saving
   if(saveplot){
     pt <- unique(gridded_traps$season_month_interval)
-    ggsave(here('wdfw','maps',paste0(pt,'.png')),map_out,w=6,h=5)
+    ggsave(here('wdfw','maps','OR','OR M2 2weekly maps',paste0(pt,'.png')),map_out,w=6,h=5)
   }
   return(map_out)
 }
@@ -250,23 +249,23 @@ proc.time()-tm
 #------------------------------------------------------------------------------
 #------------------------------------------------------------------------------
 
-# Making summary maps for May - Sep 15 period
+# Making summary maps for May - Sep 15 period --> in OR season ends on 14 Aug each year
 
 ## Cleaned and summarized, simulated crab trap data
 adj_summtraps <- read_rds(here::here('wdfw','data','adj_summtraps.rds'))
 
-# create a column in df to indicate whether data fall between May1 and Sep15
+# create a column in df to indicate whether data fall between May1 and Aug14
 # the 'periods' included between May1 and Sep15 are:
-# May_1, May-2, June_1, June_2, July_1, July_2, AUgust_1, August_2, September_1
+# May_1, May-2, June_1, June_2, July_1, July_2, August_1
 
-adj_summtraps_MaySep <- adj_summtraps %>% 
-  mutate(is_May1_Sep15 = 
-           ifelse(month_interval %in% c('May_1', 'May_2', 'June_1', 'June_2', 'July_1', 'July_2', 'August_1', 'August_2', 'September_1')
+adj_summtraps_MayAug <- adj_summtraps %>% 
+  mutate(is_May1_Aug14 = 
+           ifelse(month_interval %in% c('May_1', 'May_2', 'June_1', 'June_2', 'July_1', 'July_2', 'August_1')
                   ,'Y', 'N')) %>% 
-  filter(is_May1_Sep15 == 'Y')
+  filter(is_May1_Aug14 == 'Y')
 
 # average M1 and M2 trap density for each grid cell for May-Sep period
-MaySep_summtrapsWA <- adj_summtraps_MaySep %>%
+MayAug_summtrapsOR <- adj_summtraps_MayAug %>%
   group_by(season, GRID5KM_ID, grd_x, grd_y, AREA) %>%  
   summarise(
     sum_M1_trapdens = sum(M1_trapdens),
@@ -283,70 +282,70 @@ MaySep_summtrapsWA <- adj_summtraps_MaySep %>%
     #M2_percentile_25th = quantile(M2_trapdens, probs=0.25, na.rm=TRUE),
     #M2_percentile_025th = quantile(M2_trapdens, probs=0.025, na.rm=TRUE),
   )
-glimpse(MaySep_summtrapsWA)
+glimpse(MayAug_summtrapsOR)
 
 #--------------------------
 # If want to create non-confidential maps (do not show data if < 3 vessels in grid)
 
-conf_MaySep_summtrapsWA <- MaySep_summtrapsWA %>%
+conf_MayAug_summtrapsOR <- MayAug_summtrapsOR %>%
   mutate(is_confidential=ifelse(sum_nvessels<3,T,F))
 
-# use conf_MaySep_summtrapsWA as input in mapping loop, if want cells with < 3 vessels to be gray
-conf_MaySep_summtrapsWA <- conf_MaySep_summtrapsWA %>% 
+# use conf_MayAug_summtrapsOR as input in mapping loop, if want cells with < 3 vessels to be gray
+conf_MayAug_summtrapsOR <- conf_MayAug_summtrapsOR %>% 
   mutate(mean_M1_trapdens = ifelse(is_confidential,NA,mean_M1_trapdens),
          mean_M2_trapdens = ifelse(is_confidential,NA,mean_M2_trapdens)
   )
 
-# or use conf_MaySep_summtrapsWA2 as input in mapping loop, if want cells with < 3 vessels to be fully removed
-conf_MaySep_summtrapsWA2 <-  conf_MaySep_summtrapsWA %>%
+# or use conf_MayAug_summtrapsOR2 as input in mapping loop, if want cells with < 3 vessels to be fully removed
+conf_MayAug_summtrapsOR2 <-  conf_MayAug_summtrapsOR %>%
   filter(is_confidential == FALSE)
 #----------------------------
 
-# map May 1- Sep 15
+# map May 1- Aug 14
 # currently for M2 only, but can be edited to map M1 as well
 
 # Figure out good trap density scale
-MaySep_summtrapsWA %>%
+MayAug_summtrapsOR %>%
   ggplot()+
   geom_density(aes(mean_M2_trapdens))
-
+#when using data NOT filtered for SpatialFlag, 60 is a good max, when filtered for SpatialFlag the max value is 35
 
 # change input file if want to make non-confidential maps (currently showing confidential data for grids with < 3 vessels)
 
-map_maysep <- function(MaySep_summtrapsWA,saveplot=TRUE){
+map_mayaug <- function(MayAug_summtrapsOR,saveplot=TRUE){
   
   # labels for plot titles
-  season_label=unique(MaySep_summtrapsWA$season)
+  season_label=unique(MayAug_summtrapsOR$season)
   
-  bbox = c(800000,1650000,1013103,1970000)
+  bbox = c(800000,1170000,1025000,1920000) #adjusted for OR data
   
-  MaySep_map_out <- MaySep_summtrapsWA %>% 
+  MayAug_map_out <- MayAug_summtrapsOR %>% 
     ggplot()+
     geom_tile(aes(grd_x,grd_y,fill=mean_M2_trapdens),na.rm=T,alpha=0.8)+
     geom_sf(data=coaststates,col=NA,fill='gray50')+
-    geom_sf(data=MA_shp,col="black", size=0.5, fill=NA)+
-    geom_sf(data=QSMA_shp,col="black", linetype = "11", size=0.5, fill=NA)+
-    scale_fill_viridis(na.value='grey70',option="C",limits=c(0,50),breaks=c(0, 25,50),oob=squish)+
+    #geom_sf(data=MA_shp,col="black", size=0.5, fill=NA)+
+    #geom_sf(data=QSMA_shp,col="black", linetype = "11", size=0.5, fill=NA)+
+    scale_fill_viridis(na.value='grey70',option="C",limits=c(0,30),breaks=c(0, 30),oob=squish)+
     coord_sf(xlim=c(bbox[1],bbox[3]),ylim=c(bbox[2],bbox[4]))+
     #if you do NOT want to show lat/lon lines on the map, use the below line instead:
     #coord_sf(xlim=c(bbox[1],bbox[3]),ylim=c(bbox[2],bbox[4]),datum=NA)+
-    labs(x='',y='',fill='Avg. trap density\nper sq. km',title=paste0('May 1 - Sep 15\n',season_label))
+    labs(x='',y='',fill='Avg. trap density\nper sq. km',title=paste0('May 1 - Aug 14\n',season_label))
   
   # saving
   if(saveplot){
-    pt <- unique(MaySep_summtrapsWA$season)
-    ggsave(here('wdfw','maps',paste0('May 1 - Sep 15 ',pt,'.png')),MaySep_map_out,w=6,h=5)
+    pt <- unique(MayAug_summtrapsOR$season)
+    ggsave(here('wdfw','maps','OR','OR May-Aug maps',paste0('May 1 - Aug 14 ',pt,'.png')),MayAug_map_out,w=6,h=5)
   }
-  return(MaySep_map_out)
+  return(MayAug_map_out)
 }
 
 # Loop and save maps
 # change input file here if want to make non-confidential maps (currently showing confidential data for grids with < 3 vessels)
 tm <- proc.time()
-all_maps <- purrr::map(unique(MaySep_summtrapsWA$season),function(x){
-  MaySep_summtrapsWA %>% 
+all_maps <- purrr::map(unique(MayAug_summtrapsOR$season),function(x){
+  MayAug_summtrapsOR %>% 
     filter(season==x) %>% 
-    map_maysep()
+    map_mayaug()
 })
 proc.time()-tm
 
