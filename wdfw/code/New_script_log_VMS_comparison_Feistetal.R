0eab34dfe5d9ce9afa9079dd6b900bf85deb0f5c
Leena <leena.riekkola@noaa.gov>
2021-10-22 14:33:26 -0700

    export non-confidential version of logbook data, tidy code

--- wdfw/code/New_script_log_VMS_comparison_Feistetal.R
+++ wdfw/code/New_script_log_VMS_comparison_Feistetal.R
@@ -1,4 +1,8 @@
-#Summarise logbook data in order to compare with VMS maps in Feist et al 2021 Fig 2
+
+# Summarise logbook data in order to compare with VMS maps in Feist et al 2021 Fig 2
+# Data provided to Blake for posting maps on CCIEA website
+
+#-----------------------------------------------------------------------------------
 
 library(tidyverse)
 library(sf)
@@ -31,6 +35,7 @@ plot_theme <-   theme_minimal()+
 theme_set(plot_theme)
 options(dplyr.summarise.inform = FALSE)
 
+#-----------------------------------------------------------------------------------
 
 # Read in spatial grid data 
 # example spatial grid - 5x5 grid shapefile
@@ -47,19 +52,18 @@ grd_area_key <- grd %>%
 coaststates <- ne_states(country='United States of America',returnclass = 'sf') %>% 
   filter(name %in% c('California','Oregon','Washington','Nevada')) %>%  
   st_transform(st_crs(grd))
+
 #------------------------------------------------------------------------------
 
-# Read in trap density dataframe - note that currently this has not been fixed for the
-# issue of 'too short and too long' stringlines
-#adj_summtraps <- read_rds(here::here('wdfw','data','adj_summtraps.rds'))
+# Read in trap density dataframe 
 adj_summtraps <- read_rds(here::here('wdfw','data','adj_summtraps_2013_2020.rds'))
 
 # the df has trap density in a grid cell in a 2-week period
 # so step 1 is to summarise data on the same time steps as Feist et al 2021 Fig 2
 # note that logbook df currently covers 2013-2019 period, so not a perfect overlap with Feist et al 2021 Fig 2
 
-# summarising trap densities on same time step as Feist et al 2021 Fig 2 --> average trap density
-# in each grid cell across the chose time period
+# summarising trap densities on same time step as Feist et al 2021 Fig 2: 
+# average trap density in each grid cell across the chose time period
 # code is modified from script 6 - Making summary maps for May - Sep 15 period
 
 # create a column in df to indicate when data fall in the time steps used in Feist et al 2021 Fig 2
@@ -82,33 +86,25 @@ adj_summtraps_intervals <- adj_summtraps %>%
 glimpse(adj_summtraps_intervals)
 
 
-# average M2 trap density for each grid cell for chosen time period
-avg_trap_dens_adj_summtraps_intervals <- adj_summtraps_intervals %>%
+# average (M2) trap density for each grid cell for chosen time period
+avg_trap_dens_intervals <- adj_summtraps_intervals %>%
   group_by(month_interval, GRID5KM_ID, grd_x, grd_y) %>%  #do not group by AREA as some grid cells that overlap land appear twice
   summarise(
-    #sum_M1_trapdens = sum(M1_trapdens),
     sum_M2_trapdens = sum(M2_trapdens),
-    
-    #these are not the correct way to determine confidentiality. see below
-    #sum_nvessels = sum(nvessels),
-    #avg_nvessels = mean(nvessels),# include this for creating non-confidential maps -- should this be avg instead of sum?
-    
     number_obs = n(), #no. of grid cells being used for averaging
-    #mean_M1_trapdens = sum_M1_trapdens/number_obs,
     mean_M2_trapdens = sum_M2_trapdens/number_obs
   )
-glimpse(avg_trap_dens_adj_summtraps_intervals)
-
-
+glimpse(avg_trap_dens_intervals)
 
 #----------------------------------------------------------------------------------------------
-# # If want to create non-confidential maps (do not show data if < 3 vessels in grid)
 
-#I think this is the correct way of figuring if more than 3 unique vessels were in a grid cell in a given period
-#bring in data as points, not summarised by grid cell
-traps_g_all_logs <- read_rds(here::here('wdfw', 'data','traps_g_license_logs_2013_2019.rds'))
+# If want to create non-confidential maps (do not show data if < 3 vessels in grid)
 
-#for each point record assign the month_interval as per Feist et al Fig 2
+# this is the correct way of figuring if more than 3 unique vessels were in a grid cell in a given time period
+# bring in logbook data as points, not summarised by grid cell
+traps_g_all_logs <- read_rds(here::here('wdfw', 'data','traps_g_license_all_logs_2013_2020.rds'))
+
+# for each point record assign the month_interval as per Feist et al Fig 2
 logs_all_x <- traps_g_all_logs %>% 
   st_set_geometry(NULL) %>% 
   mutate(m=month(SetDate),d=day(SetDate),period=ifelse(d<=15,1,2)) %>% 
@@ -130,80 +126,114 @@ logs_all_x <- traps_g_all_logs %>%
   ) %>% 
   filter(!is.na(month_interval))
 
-#count number of unique vessels that used a given grid cell within a given month_interval
+# count number of unique vessels that used a given grid cell within a given month_interval
 logs_all_nvessels <- logs_all_x %>% 
   group_by(month_interval, GRID5KM_ID,grd_x,grd_y) %>% 
   summarise(
     nvessels=n_distinct(Vessel,na.rm=T)) 
 
-#join the new, correct, number of unique vessels in a grid in an interval into gridded df
-avg_trap_dens_adj_summtraps_intervals %<>%
+# join the new (correct) number of unique vessels in a grid in an interval into gridded df
+avg_trap_dens_intervals %<>%
   left_join(logs_all_nvessels,by=c("month_interval","GRID5KM_ID", "grd_x", "grd_y"))
 
-#If fewer than 3 unique vessels in a grid, that should be removed
-conf_avg_trap_dens_adj_summtraps_intervals4 <- avg_trap_dens_adj_summtraps_intervals %>%
+# If fewer than 3 unique vessels in a grid, that should be removed
+non_conf_avg_trap_dens_intervals_2 <- avg_trap_dens_intervals %>%
   mutate(is_confidential=ifelse(nvessels<3,T,F)) 
-conf_avg_trap_dens_adj_summtraps_intervals5 <-  conf_avg_trap_dens_adj_summtraps_intervals4 %>%
+non_conf_avg_trap_dens_intervals_3 <-  non_conf_avg_trap_dens_intervals_2 %>%
   filter(is_confidential == FALSE)
 
+#remove few excess columns to make things tidier for Blake
+non_conf_avg_trap_dens_intervals_3 <-  non_conf_avg_trap_dens_intervals_3 %>%
+  select(-c(sum_M2_trapdens, number_obs, nvessels, is_confidential))
+  
+#write_csv(non_conf_avg_trap_dens_intervals_3,here::here('wdfw', 'data','Blake VMS and log comparison', "Dungeness 4mon 5km WA Logbook_avg trap dens_NON_CONFIDENTIAL_long.csv"))
+
+
+#get logbook data to wider format for Blake
+non_conf_avg_trap_dens_intervals_3_wide <- non_conf_avg_trap_dens_intervals_3 %>%
+  pivot_wider(names_from = month_interval,
+              names_sep = "_",
+              values_from = c(mean_M2_trapdens)
+  ) 
+
+#write_csv(non_conf_avg_trap_dens_intervals_3_wide,here::here('wdfw', 'data', 'Blake VMS and log comparison', "Dungeness 4mon 5km WA Logbook_avg trap dens_NON_CONFIDENTIAL_wide.csv"))
 
 
 #----------------------------------------------------------------------------------------------
 # # map 
 # # Figure out good trap density scale
-# conf_avg_trap_dens_adj_summtraps_intervals5 %>%
+# non_conf_avg_trap_dens_intervals_3 %>%
 #   ggplot()+
 #   geom_density(aes(mean_M2_trapdens))
 # 
 # 
-# # change input file if want to make non-confidential maps (currently showing confidential data for grids with < 3 vessels)
-# 
+# # change input file if want to make non-confidential maps (currently NOT showing confidential data)
+# # note that in an earlier iteration normalised data, hence below can see naming convention _normalised
+#
 # map_log_vms <- function(avg_trap_dens_adj_summtraps_intervals_normalised,saveplot=TRUE){
-#   
+# 
 #   # labels for plot titles
 #   season_label=unique(avg_trap_dens_adj_summtraps_intervals_normalised$month_interval)
-#   
+# 
 #   bbox = c(800000,1650000,1013103,1970000)
-#   
-#   log_vms_map_out <- avg_trap_dens_adj_summtraps_intervals_normalised %>% 
+# 
+#   log_vms_map_out <- avg_trap_dens_adj_summtraps_intervals_normalised %>%
 #     ggplot()+
-#     geom_tile(aes(grd_x,grd_y,fill=trapdens_norm_within_interval ),na.rm=T,alpha=0.8)+
+#     geom_tile(aes(grd_x,grd_y,fill=mean_M2_trapdens),na.rm=T,alpha=0.8)+
 #     geom_sf(data=coaststates,col=NA,fill='gray50')+
 #     #geom_sf(data=MA_shp,col="black", size=0.5, fill=NA)+
 #     #geom_sf(data=QSMA_shp,col="black", linetype = "11", size=0.5, fill=NA)+
-#     #scale_fill_viridis(na.value='grey70',option="C",limits=c(0,60),breaks=c(0, 20, 40, 60),oob=squish)+
-#     scale_fill_viridis(na.value='grey70',option="C",limits=c(0,1),breaks=c(0, 0.5, 1),oob=squish)+
+#     scale_fill_viridis(na.value='grey70',option="C",limits=c(0,60),breaks=c(0, 20, 40, 60),oob=squish)+
+#     #scale_fill_viridis(na.value='grey70',option="C",limits=c(0,1),breaks=c(0, 0.5, 1),oob=squish)+
 #     coord_sf(xlim=c(bbox[1],bbox[3]),ylim=c(bbox[2],bbox[4]))+
 #     #if you do NOT want to show lat/lon lines on the map, use the below line instead:
 #     #coord_sf(xlim=c(bbox[1],bbox[3]),ylim=c(bbox[2],bbox[4]),datum=NA)+
 #     labs(x='',y='',fill='Avg. trap density\nper sq. km',title=season_label)
-#   
+# 
 #   # saving
 #   if(saveplot){
 #     pt <- unique(avg_trap_dens_adj_summtraps_intervals_normalised$month_interval)
-#     ggsave(here('wdfw','maps',paste0('log_vms',pt,'.png')),log_vms_map_out,w=6,h=5)
+#     ggsave(here('wdfw','maps','Blake_log_vms_comparison',paste0('log_vms',pt,'.png')),log_vms_map_out,w=6,h=5)
 #   }
 #   return(log_vms_map_out)
 # }
 # 
 # # Loop and save maps
-# # change input file here if want to make non-confidential maps (currently showing confidential data for grids with < 3 vessels)
+# # change input file here if want to make non-confidential maps 
+# # currently using "non_conf_avg_trap_dens_intervals_3" here to make non-confidential maps (grids with < 3 vessels not shown)
 # tm <- proc.time()
-# all_maps <- purrr::map(unique(avg_trap_dens_adj_summtraps_intervals_normalised$month_interval),function(x){
-#   avg_trap_dens_adj_summtraps_intervals_normalised %>% 
-#     filter(month_interval==x) %>% 
+# all_maps <- purrr::map(unique(non_conf_avg_trap_dens_intervals_3$month_interval),function(x){
+#   non_conf_avg_trap_dens_intervals_3 %>%
+#     filter(month_interval==x) %>%
 #     map_log_vms()
 # })
 # proc.time()-tm
 
 
-#----------------------------------------
 
-#read in Blakes VMS data and normalise
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#----------------------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------------------
+### OLD ### older iterations of working with log and VMS data for CCIEA maps, including normalising (making data range 0-1)
+
+# read in Blakes VMS data and normalise
 Dungeness_4mon_5km_attribute_table_CONFIDENTIAL <- read_csv(here::here('wdfw','data','Dungeness 4mon 5km attribute table CONFIDENTIAL.csv'))
 
-#If need to make VMS into long format:
-#pivot_longer(df, starts_with("_")) -- so don't have to type all column names
+# If need to make VMS into long format:
+# pivot_longer(df, starts_with("_")) -- so don't have to type all column names
 longdata <- pivot_longer(Dungeness_4mon_5km_attribute_table_CONFIDENTIAL, starts_with("DUN"))
 #longdata <- pivot_longer(Dungeness_4mon_5km_attribute_table_CONFIDENTIAL, c(DUN_NF1314, DUN_MJ2014, DUN_JO2014, DUN_NF1415, DUN_MJ2015, DUN_JO2015, DUN_NF1516, DUN_MJ2016))
 
@@ -218,8 +248,8 @@ longdata <- pivot_longer(Dungeness_4mon_5km_attribute_table_CONFIDENTIAL, starts
 #           DUN_MJ2016_norm = scales::rescale(DUN_MJ2016 , to=c(0,1))
 #           )
 
-#Blake requires the resulting df in a 'wide' format
-#but I also saved a long verion before running pivot_wider()
+# Blake requires the resulting df in a 'wide' format
+# but I also saved a long verion before running pivot_wider()
 #write_csv(Dungeness_4mon_5km_attribute_table_CONFIDENTIAL_normalised,here::here('wdfw', 'data', "Dungeness_4mon_5km_attribute_table_CONFIDENTIAL_normalised_within_between_long.csv"))
 
 Dungeness_4mon_5km_attribute_table_CONFIDENTIAL_normalised <- longdata %>% 
@@ -254,14 +284,15 @@ Dungeness_4mon_5km_attribute_table_CONFIDENTIAL_normalised_wide <-  Dungeness_4m
 #write_csv(Dungeness_4mon_5km_attribute_table_CONFIDENTIAL_normalised_wide,here::here('wdfw', 'data', "Dungeness_4mon_5km_attribute_table_CONFIDENTIAL_normalised_within_between.csv"))
 
 
-#----------------------------------------
-# Normalise trap density within intervals, but also within 13/14 11-02 and 2016 03-06 period
-#for now use 'confidential' data, no need to remove grids with < 3 vessels
+#------------------------------------
+
+# Normalise trap density within intervals, but also within the full 13/14 11-02 and 2016 03-06 period
+# for now use 'confidential' data, no need to remove grids with < 3 vessels
 avg_trap_dens_adj_summtraps_intervals_normalised <- avg_trap_dens_adj_summtraps_intervals %>% 
   group_by(month_interval) %>% 
   mutate(trapdens_norm_within_interval = scales::rescale(mean_M2_trapdens , to=c(0,1)))
 
-#need to ungroup for rescale() to work correctly
+# need to ungroup for rescale() to work correctly
 avg_trap_dens_adj_summtraps_normalised_within_between <- avg_trap_dens_adj_summtraps_intervals_normalised %>%
   ungroup() %>% 
   mutate(trapdens_norm_between_intervals = scales::rescale(mean_M2_trapdens , to=c(0,1)))
@@ -269,8 +300,8 @@ avg_trap_dens_adj_summtraps_normalised_within_between <- avg_trap_dens_adj_summt
 #write_csv(avg_trap_dens_adj_summtraps_normalised_within_between,here::here('wdfw', 'data', "avg_trap_dens_normalised_within_between_long.csv"))
 
 
-#get logbook data to wider format for Blake
-#remove some excess columns and rename some to make things tidier
+# get logbook data to wider format for Blake
+# remove some excess columns and rename some to make things tidier
 avg_trap_dens_adj_summtraps_normalised_within_between_tidy <-  avg_trap_dens_adj_summtraps_normalised_within_between %>% 
   select(c(GRID5KM_ID,mean_M2_trapdens, trapdens_norm_within_interval, trapdens_norm_between_intervals, month_interval)) %>% 
   rename(mean_trapdens=mean_M2_trapdens, trapdens_norm_within = trapdens_norm_within_interval, trapdens_norm_between = trapdens_norm_between_intervals)
@@ -284,11 +315,10 @@ avg_trap_dens_normalised_within_between_wide <- avg_trap_dens_adj_summtraps_norm
 #write_csv(avg_trap_dens_normalised_within_between_wide,here::here('wdfw', 'data', "avg_trap_dens_normalised_within_between_wide.csv"))
 
 
+#--------------------------------
+# make time series plots
 
-#------------------------------------------
-#make time series plots
-
-#read in the long dataframes of VMS pings and logs with normalised data
+# read in the long dataframes of VMS pings and logs with normalised data
 
 logs_norm <- read_csv(here::here('wdfw','data','avg_trap_dens_normalised_within_between_long.csv'))
 VMS_norm <- read_csv(here::here('wdfw','data','Dungeness_4mon_5km_attribute_table_CONFIDENTIAL_normalised_within_between_long.csv'))
@@ -297,7 +327,7 @@ logs_norm <- logs_norm %>%
   mutate(month_interval = factor(month_interval, 
                                  levels = c('13_14 11-02','2014 03-06','2014 07-10','14_15 11-02','2015 03-06','2015 07-10','15_16 11-02','2016 03-06'))) 
 
-#change some column names etc in VMS df to make it similar to log df
+# change some column names etc in VMS df to make it similar to log df
 VMS_norm <-  VMS_norm %>% 
   rename(VMS_pings = value,  month_interval= name) %>% 
   mutate(
@@ -314,11 +344,10 @@ VMS_norm <-  VMS_norm %>%
   ) %>% 
   mutate(month_interval = factor(month_interval, 
         levels = c('13_14 11-02','2014 03-06','2014 07-10','14_15 11-02','2015 03-06','2015 07-10','15_16 11-02','2016 03-06'))) 
-  
 
 
 #-----------------------------------------
-#look at the distribution of normalised values
+# look at the distribution of normalised values
 
 plot1 <- VMS_norm %>%
   ggplot() + 
@@ -329,9 +358,9 @@ plot1 <- VMS_norm %>%
   ggtitle('Distribution of normalised ping values')
 plot1
 
-#realised that VMS df has lots of 0s. The equivalent in logs are NAs which don't get plotted, so comparison between the two is not quite right. 
-#So need to remove 0s from VMS df, and THEN normalize within the full 2013-2016 period we're looking at
-#also testing out transformations -- log seems the best, the 4th root
+# realised that VMS df has lots of 0s. The equivalent in logs are NAs which don't get plotted, so comparison between the two is not quite right. 
+# So need to remove 0s from VMS df, and THEN normalize within the full 2013-2016 period we're looking at
+# also testing out transformations -- log seems the best, the 4th root
 VMS_norm_no_zeros <- VMS_norm %>% 
   filter(VMS_pings > 0) %>% 
   mutate(VMS_norm_between_intervals_no_zeros = scales::rescale(VMS_pings, to=c(0,1)),
@@ -385,7 +414,7 @@ plot2 <- logs_norm_log %>%
 plot2
 
 #------------------------------------------
-#ts-plots
+# ts-plots
 
 # VMS_norm_max <- VMS_norm %>% 
 #   group_by(month_interval) %>% 
@@ -476,9 +505,6 @@ map_out <- plot_grid(tsplot1,tsplot2,nrow=1)
 
 #line only
 
-
-
-
 tsplot3 <-  
   ggplot()+
   #geom_line(data=VMS_norm_no_zeros_max, aes(x=month_interval,y=max, group=1))+
@@ -560,13 +586,13 @@ tsplot3b
 
 
 
-#--------------------------------------------------------------------------
-#--------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------
 
-#re-do plots with blake's data file
+# re-do plots with blake's data file
 
 blake_df <- read_csv(here::here('wdfw','data', 'Blake VMS and log comparison','UPDATED_Dungeness 4mon 5km WA VMS & Logbook attribute table CONFIDENTIAL.csv'))
-#blake's df is in wide format, convert to long
+# blake's df is in wide format, convert to long
 
 log_df <- blake_df %>% 
   select(GRID5KM_ID:LOG_MEMBER, MEMBERSHIP)
@@ -718,8 +744,8 @@ scatter_plot_2
 
 
 
-#--------------------------------------------------------------------------
-#--------------------------------------------------------------------------
+#----------------------------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------------------------
 
 #re-do things while excluding OR data
 
