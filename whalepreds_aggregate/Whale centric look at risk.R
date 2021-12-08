78fd6e1f92ebd561ae0418593a1fbf181388e592
leena-r <77252472+leena-r@users.noreply.github.com>
2021-12-08 12:03:01 -0800

    finding 75th percentile for hw across 2013-2020, apply to each season, look at pot denisty in good whale habitat

--- whalepreds_aggregate/Whale centric look at risk.R
+++ whalepreds_aggregate/Whale centric look at risk.R
@@ -1637,3 +1637,191 @@ ggarrange(map_blue_MaySep_075_occur,
           hjust=0
 )
 invisible(dev.off())
+
+
+#----------------------------------------------------------------------------
+#----------------------------------------------------------------------------
+#----------------------------------------------------------------------------
+# find 75th percentile value for hw across 2013-2020, in clipped (44N) extent
+
+# calculate MEAN whale values for for grids across all of 2013-2020 - for May-Sep period only
+# and then look at percentiles
+x.whale.mean_all2013_2020 <- x.whale_crab_season_v2 %>% #this is already filtered for 2013-2020
+  filter(is_May_Sep == "Y") %>% 
+  group_by(is_May_Sep, GRID5KM_ID, area_km_lno) %>%
+  summarise(
+    #hw specific
+    Mean_Humpback_dens = mean(Humpback_dens_mean, na.rm=TRUE)) %>%
+  inner_join(grid.key_N44)
+glimpse(x.whale.mean_all2013_2020)
+
+#find the 75th percentile value from across 2013-2020
+x.whale.all2013_2020_75th_quant <- x.whale.mean_all2013_2020 %>%
+  #select(Mean_Humpback_dens, Mean_Blue_occurrence) %>%
+  ungroup() %>% 
+  summarise(
+    Humpback_dens_75th = quantile(Mean_Humpback_dens, probs=0.75, na.rm=TRUE)
+  ) 
+glimpse(x.whale.all2013_2020_75th_quant)
+# 0.02466905
+
+
+#apply percentile value to each season
+x.whale.mean_by_season <- x.whale_crab_season_v2 %>%
+  filter(is_May_Sep == "Y") %>% 
+  group_by(season, is_May_Sep, GRID5KM_ID, area_km_lno) %>%
+  summarise(
+    Mean_Humpback_dens = mean(Humpback_dens_mean, na.rm=TRUE)) %>%
+  inner_join(grid.key_N44)
+glimpse(x.whale.mean_by_season)
+
+x.whale.all2013_2020_MaySep_quant_joined <- x.whale.mean_by_season %>% 
+  cbind(x.whale.all2013_2020_75th_quant)
+glimpse(x.whale.all2013_2020_MaySep_quant_joined)
+
+x.whale.all2013_2020_MaySep_good_habitats <- x.whale.all2013_2020_MaySep_quant_joined %>% 
+  ungroup() %>% 
+  mutate(HW_is_75th_or_higher = ifelse(Mean_Humpback_dens > Humpback_dens_75th, 'Y', 'N')) %>%
+  inner_join(grid.key_N44)
+glimpse(x.whale.all2013_2020_MaySep_good_habitats)
+
+
+#------------------
+# select the below section, tick 'in selection' and change the below values
+# change 2013_2014 #3
+# change 2013-2014 #4
+
+#map all seasons May_Sep good whale habitats with fishery footprint for that season's May-Sep
+dissolved_2019_2020_MaySep <- read_rds(here::here('wdfw','data','dissolved_2019_2020_MaySep_WA_fishery_footprint.rds'))
+
+# grab a base map
+rmap.base <- c(
+  st_geometry(ne_states(country = "United States of America", returnclass = "sf")),   ne_countries(scale = 10, continent = "North America", returnclass = "sf") %>%
+    filter(admin %in% c("Canada", "Mexico")) %>%
+    st_geometry() %>%
+    st_transform(st_crs(grid.5km.lno))
+)
+
+#bbox
+bbox = c(-127,43.5,-120,49) 
+
+
+hw_subset_MaySep <- x.whale.all2013_2020_MaySep_good_habitats %>% 
+  #select season to map 
+  filter(season == "2019-2020") %>% 
+  filter(!is.na(HW_is_75th_or_higher)) %>% 
+  filter(HW_is_75th_or_higher == 'Y')
+
+
+map_hump_MaySep_75th <- ggplot() + 
+  geom_sf(data=sf::st_as_sf(hw_subset_MaySep), 
+          aes(fill=HW_is_75th_or_higher,
+              col=HW_is_75th_or_higher
+          )
+  ) +
+  geom_sf(data=rmap.base,col=NA,fill='gray50') +
+  #scale_fill_viridis(na.value=NA,option="D",name="Humpback Whale\nDensity",breaks=seq(0,0.04,by=0.01),limits=c(0.0,0.04),oob=squish) + 
+  #scale_color_viridis(na.value=NA,option="D",name="Humpback Whale\nDensity",breaks=seq(0,0.04,by=0.01),limits=c(0.0,0.04),oob=squish) + 
+  scale_fill_manual(values = c("mediumspringgreen"), name = "Good whale habitat", labels = c("Yes")) +
+  scale_color_manual(values = c("mediumspringgreen"), name = "Good whale habitat", labels = c("Yes")) +
+  geom_sf(data = dissolved_2019_2020_MaySep, color = 'black',size=1, fill = NA) +
+  ggtitle("May-Sep 2019-2020 \ngood HW habitat (75th+) \nspatially clip at 44N \nwith 2019-2020 May-Sep fishery footprint") +
+  coord_sf(xlim=c(bbox[1],bbox[3]),ylim=c(bbox[2],bbox[4])) +
+  #coord_sf(xlim=c(grid5km_bbox[1],grid5km_bbox[3]),ylim=c(grid5km_bbox[2],grid5km_bbox[4])) + 
+  theme_minimal() + #theme_classic() +
+  theme(text=element_text(family="sans",size=10,color="black"),
+        legend.text = element_text(size=10),
+        axis.title=element_text(family="sans",size=14,color="black"),
+        axis.text=element_text(family="sans",size=8,color="black"),
+        panel.grid.major = element_line(color="gray50",linetype=3),
+        axis.text.x.bottom = element_text(angle=45, vjust = 0.5),
+        strip.text = element_text(size=14),
+        title=element_text(size=16)
+  )
+map_hump_MaySep_75th
+
+png(paste0(path_figures, "/good_wh_habitat_MaySep_75th_across_20132020_applied to each season_2019-2020_spatially_clipped.png"), width = 14, height = 10, units = "in", res = 300)
+ggarrange(map_hump_MaySep_75th,
+          ncol=1,
+          nrow=1,
+          legend="top",
+          labels="auto",
+          vjust=8,
+          hjust=0
+)
+invisible(dev.off())
+
+
+#----------------
+#Then try to look at what trap density was in the good (75th) hw habitat -- will need to check code
+#bring in fishing data 
+path.fish_WA <- "C:/Users/Leena.Riekkola/Projects/raimbow/wdfw/data/adj_summtraps_2013_2020.rds"
+x.fish_WA <- readRDS(path.fish_WA) %>% 
+  mutate(is_May_Sep = 
+           ifelse(month_name %in% c('May', 'June', 'July', 'August', 'September')
+                  ,'Y', 'N'))
+
+# average trap density (and count?) for each grid cell for May-Sep period
+x.fish_WA_MaySep <- x.fish_WA %>% 
+  filter(is_May_Sep == "Y") %>% 
+  group_by(season, GRID5KM_ID, grd_x, grd_y, AREA) %>%  
+  summarise(
+    sum_M2_trapdens = sum(M2_trapdens, na.rm=TRUE),
+    number_obs = n(), #no. of grid cells being used for averaging
+    mean_trapdens = sum_M2_trapdens/number_obs
+    #sum_M2_tottraps  = sum(M2_tottraps, na.rm=TRUE),
+    #mean_tottraps = sum_M2_tottraps/number_obs
+  ) %>% 
+  #and drop some columns so that after join things are little tidier
+  select(season, GRID5KM_ID, mean_trapdens) #, mean_tottraps
+glimpse(x.fish_WA_MaySep)
+
+
+x.whale.2013_2020_MaySep_good_habitats_fishing <- x.whale.all2013_2020_MaySep_good_habitats %>% 
+  left_join(x.fish_WA_MaySep, by=c('season', 'GRID5KM_ID'))
+glimpse(x.whale.2013_2020_MaySep_good_habitats_fishing)
+
+
+summary_75th_HW_habitat_fishing <- x.whale.2013_2020_MaySep_good_habitats_fishing %>% 
+  filter(HW_is_75th_or_higher == 'Y') %>% 
+  group_by(season) %>% 
+  summarise(trapdens_mean = mean(mean_trapdens, na.rm=TRUE),
+            trapdens_median = median(mean_trapdens, na.rm=TRUE)
+            #tottraps_mean = mean(mean_tottraps, na.rm=TRUE),
+            #tottraps_median = median(mean_tottraps, na.rm=TRUE)
+  )
+glimpse(summary_75th_HW_habitat_fishing)  
+
+
+ts_fishing_in_75th_hw_habitat <- ggplot(summary_75th_HW_habitat_fishing, aes(x=season)) + 
+  geom_line(aes(y = trapdens_mean, group = 1)) + 
+  geom_point(aes(y = trapdens_mean, group = 1), size=2) + 
+  geom_line(aes(y = trapdens_median, group = 1), color = "darkred", linetype="twodash") + 
+  geom_point(aes(y = trapdens_median, group = 1), color = "darkred", size=2) + 
+  ylab("Trap density") + 
+  xlab("Season") +
+  ggtitle("May-Sep trap density \nmean = solid line, median = dashed line \nin good (75th) HW habitat \n(defined across 2013-2020)") +
+  theme_classic() +
+  theme(legend.title = element_blank(),
+        #title = element_text(size = 26),
+        legend.text = element_text(size = 20),
+        legend.position = c(.15, .85),
+        axis.text.x = element_text(hjust = 1,size = 12, angle = 60),
+        axis.text.y = element_text(size = 12),
+        axis.title = element_text(size = 12),
+        strip.text = element_text(size=12),
+        strip.background = element_blank(),
+        strip.placement = "left"
+  )
+ts_fishing_in_75th_hw_habitat
+
+png(paste0(path_figures, "/ts_fishing_in_75th_hw_habitat_MaySep.png"), width = 17, height = 10, units = "in", res = 300)
+ggarrange(ts_fishing_in_75th_hw_habitat,
+          ncol=1,
+          nrow=1,
+          legend="top",
+          labels="auto",
+          vjust=8,
+          hjust=0
+)
+invisible(dev.off())
