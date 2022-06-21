03894a0e54e1e13bfe99c09dd4f737d293650443
leena-r <77252472+leena-r@users.noreply.github.com>
2022-06-21 09:44:22 -0700

    updating figure (maps added to figure)

--- whalepreds_aggregate/Humpback whale centric look at risk__separate JulSep and MaySep.R
+++ whalepreds_aggregate/Humpback whale centric look at risk__separate JulSep and MaySep.R
@@ -939,15 +939,17 @@ summary(mod1_hump_overlap_MaySep)
 
 
 ts_fishing_in_75th_hw_habitat_JulSep_MaySep <- ggplot(data=summary_75th_HW_habitat_fishing_JulSep, aes(x=season, y = risk_sum, group = 1, color='Jul-Sep')) +
-  geom_line(size=2) +
+  geom_line(size=2,  linetype = "dashed") +
   geom_point(size=5) +
   geom_line(data=summary_75th_HW_habitat_fishing_MaySep, aes(x=season, y = risk_sum, group = 1, color='May-Sep'), size=2) +
   geom_point(data=summary_75th_HW_habitat_fishing_MaySep, aes(x=season, y = risk_sum, group = 1, color='May-Sep'), size=5) +
+  ylim(0, 13) + 
   #ylab("Summed humpback whale risk") +
   ylab("Risk") +
-  xlab("Season") +
+  #xlab("Season") +
   xlab("") +
-  scale_color_manual(name="", values = c("#00bab5","#73377e")) +
+  #scale_color_manual(name="", values = c("#00bab5","#73377e")) +
+  scale_color_manual(name="", values = c("black","black")) +
   scale_x_discrete(labels=c("2013-2014" = "2014",
                             "2014-2015" = "2015",
                             "2015-2016" = "2016",
@@ -961,17 +963,17 @@ ts_fishing_in_75th_hw_habitat_JulSep_MaySep <- ggplot(data=summary_75th_HW_habit
         legend.text = element_text(size = 40),
         legend.position = "none",
         #legend.position = c(.89, .92),
-        axis.text.x = element_text(hjust = 0.5,size = 40, angle = 0),
-        axis.text.y = element_text(size = 40),
-        axis.title = element_text(size = 50),
-        strip.text = element_text(size=40),
+        axis.text.x = element_text(hjust = 0.5,size = 40, angle = 0,color="black"),
+        axis.text.y = element_text(size = 35,color="black"),
+        axis.title = element_text(size = 40),
+        strip.text = element_text(size=35),
         strip.background = element_blank(),
         strip.placement = "left"
   )
 ts_fishing_in_75th_hw_habitat_JulSep_MaySep
 
 # SAVE FIGURE -- Figure 5
-# png(paste0(path_figures, "/risk_75th_HW_habitat_JulSep_MaySep_NORM.png"), width = 17, height = 10, units = "in", res = 360)
+# png(paste0(path_figures, "/risk_75th_HW_habitat_JulSep_MaySep_NORM.png"), width = 20, height = 5, units = "in", res = 400)
 # ggarrange(ts_fishing_in_75th_hw_habitat_JulSep_MaySep,
 #           ncol=1,
 #           nrow=1,
@@ -991,14 +993,15 @@ ts_fishing_in_75th_hw_habitat_JulSep_MaySep
 
 
 ts_overlap_75th_hw_habitat_JulSep_MaySep <- ggplot(data=test_summary_75_JulSep, aes(x=season, y = n_grids, group = 1, color='Jul-Sep')) +
-  geom_line(size=2) +
+  geom_line(size=2, linetype = "dashed") +
   geom_point(size=5) +
   geom_line(data=test_summary_75_MaySep, aes(x=season, y = n_grids, group = 1, color='May-Sep'), size=2) +
   geom_point(data=test_summary_75_MaySep, aes(x=season, y = n_grids, group = 1, color='May-Sep'), size=5) +
   #ylab("Number of overlapping grids") +
   ylab("Overlap") +
-  xlab("Season") +
-  scale_color_manual(name="", values = c("#00bab5","#73377e")) +
+  xlab("") +
+  #scale_color_manual(name="", values = c("#00bab5","#73377e")) +
+  scale_color_manual(name="", values = c("black","black")) +
   scale_x_discrete(labels=c("2013-2014" = "2014",
                             "2014-2015" = "2015",
                             "2015-2016" = "2016",
@@ -1012,17 +1015,17 @@ ts_overlap_75th_hw_habitat_JulSep_MaySep <- ggplot(data=test_summary_75_JulSep, 
         legend.text = element_text(size = 22),
         #legend.position = c(.9, .85),
         legend.position = "none",
-        axis.text.x = element_text(hjust = 0.5,size = 40, angle = 0),
-        axis.text.y = element_text(size = 40),
-        axis.title = element_text(size = 50),
-        strip.text = element_text(size=40),
+        axis.text.x = element_text(hjust = 0.5,size = 35, angle = 0,color="black"),
+        axis.text.y = element_text(size = 35,color="black"),
+        axis.title = element_text(size = 40),
+        strip.text = element_text(size=35),
         strip.background = element_blank(),
         strip.placement = "left"
   )
 ts_overlap_75th_hw_habitat_JulSep_MaySep
 
 # SAVE FIGURE -- Figure 5
-# png(paste0(path_figures, "/overlap_75th_HW_habitat_JulSep_MaySep.png"), width = 17, height = 10, units = "in", res = 360)
+# png(paste0(path_figures, "/overlap_75th_HW_habitat_JulSep_MaySep.png"), width = 20, height = 5, units = "in", res = 400)
 # ggarrange(ts_overlap_75th_hw_habitat_JulSep_MaySep,
 #           ncol=1,
 #           nrow=1,
