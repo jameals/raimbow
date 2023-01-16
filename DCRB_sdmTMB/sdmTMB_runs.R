fd273b02ac467823f061683819c74e50ea47ff6d
leena-r <leena.riekkola@noaa.gov>
2023-01-17 07:31:27 +1300

    set up a script that will be used for sdmTMB runs

---
+++ DCRB_sdmTMB/sdmTMB_runs.R
@@ -0,0 +1,42 @@
+#sdmTMB runs
+
+#-------------------------------------------------------------------------------------------------
+
+library(ggplot2)
+library(tidyverse)
+library(sdmTMB)
+
+#-------------------------------------------------------------------------------------------------
+
+#read in df
+#note that this may not yet be a finished version of df - closed areas not done, some predictors might be added
+
+
+
+#-------------------------------------------------------------------------------------------------
+
+#does X and Y need to be in a projected CRS?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
