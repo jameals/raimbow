8f4a4864ed1bc6ccbabc2d10acabc6449b7b10ad
leena-r <leena.riekkola@noaa.gov>
2023-01-27 08:29:39 +1300

    fix NAs in fuel price

--- DCRB_sdmTMB/fuel_prices.R
+++ DCRB_sdmTMB/fuel_prices.R
@@ -401,17 +401,41 @@ unique(df_fuel_price$half_month)
 #------------------------------
 #2014-2015 season had no pots in August_2, but as fishery (in WA) was open
 #we need fuel prices for grid cells in that month for the presence/absence model
-interpolated_fuel_price_2014_2015
+interpolated_fuel_price_2014_2015 <- read_rds(here::here('DCRB_sdmTMB', 'data', "fuel",'v2',"interpolated_fuel_price_2014_2015.rds"))
+#interpolated_fuel_price_2014_2015
 
 interpolated_fuel_price_2014_2015_august1_september1 <- interpolated_fuel_price_2014_2015 %>% 
   filter(half_month=='August_1' | half_month=='September_1') %>% 
   group_by(GRID5KM_ID) %>% 
   summarise(weighted_fuel_pricegal = mean(weighted_fuel_pricegal)) %>% 
   mutate(season = "2014-2015", half_month = "August_2") %>% 
-  select(GRID5KM_ID, season, half_month, weighted_fuel_pricegal)
+  select(GRID5KM_ID, season, half_month, weighted_fuel_pricegal) %>% 
+  ungroup()
   
 interpolated_fuel_price_2014_2015 <- rbind(interpolated_fuel_price_2014_2015, interpolated_fuel_price_2014_2015_august1_september1)
 #write_rds(interpolated_fuel_price_2014_2015,here::here('DCRB_sdmTMB', 'data', "fuel",'v2', "interpolated_fuel_price_2014_2015.rds"))
+
+#------------------------------
+#2013-2014 needs fuel price for December_1 -- use data for December_2
+interpolated_fuel_price_2013_2014
+
+interpolated_fuel_price_2013_2014_december_fix <- interpolated_fuel_price_2013_2014 %>% 
+  filter(half_month=="December_2") %>% 
+  mutate(half_month = str_replace(half_month, "December_2", "December_1"))
+
+interpolated_fuel_price_2013_2014 <- rbind(interpolated_fuel_price_2013_2014, interpolated_fuel_price_2013_2014_december_fix)
+#write_rds(interpolated_fuel_price_2013_2014,here::here('DCRB_sdmTMB', 'data', "fuel",'v2', "interpolated_fuel_price_2013_2014.rds"))
+
+
+#2017-2018 needs fuel price for January_1 -- use data for January_2
+interpolated_fuel_price_2017_2018
+
+interpolated_fuel_price_2017_2018_january_fix <- interpolated_fuel_price_2017_2018 %>% 
+  filter(half_month=="January_2") %>% 
+  mutate(half_month = str_replace(half_month, "January_2", "January_1"))
+
+interpolated_fuel_price_2017_2018 <- rbind(interpolated_fuel_price_2017_2018, interpolated_fuel_price_2017_2018_january_fix)
+#write_rds(interpolated_fuel_price_2017_2018,here::here('DCRB_sdmTMB', 'data', "fuel",'v2', "interpolated_fuel_price_2017_2018.rds"))
 
 #------------------------------
  
@@ -443,6 +467,41 @@ study_area_grids_with_all_season_halfmonth_combos_wind_SST_fixed_depth_faults_ca
   left_join(interpolated_fuel_price_all, by=c('GRID5KM_ID', 'season', 'half_month'))
 #the cases where grid has NA for fuel price should be cases where grids were closed (season closures etc)
 #these should get removed when get around to dealing with open/closed areas (grids)
+#not all NAs are during closed periods - so needs fixing
+#discrepancy is likely due to half_monthly by landing day or Set Date
+
+#df_NAs <- study_area_grids_with_all_season_halfmonth_combos_wind_SST_fixed_depth_faults_canyon_escarp_portdist_fuel %>% 
+#  filter(is.na(weighted_fuel_pricegal))
+#df_noNAs <- study_area_grids_with_all_season_halfmonth_combos_wind_SST_fixed_depth_faults_canyon_escarp_portdist_fuel %>% 
+#  filter(!is.na(weighted_fuel_pricegal))
+
+#distinct_season_halfmonth_with_NAs <- df_NAs %>% 
+#  distinct(season, half_month)
+#season    half_month 
+#2007-2008 August_2   
+#2007-2008 September_1
+#2008-2009 August_2   
+#2008-2009 September_1
+#2012-2013 December_1 
+#2013-2014 December_1 
+#2015-2016 December_1 
+#2015-2016 December_2 
+#2016-2017 December_1 
+#2017-2018 December_1 
+#2017-2018 December_2 
+#2017-2018 January_1  
+#2018-2019 December_1 
+#2018-2019 December_2 
+#2019-2020 December_1
+
+#we can ignore 2007-2008 and 2008-2009 as those will get dropped from the analysis
+#some other cases are when fishery fully closed, so fix only times when fishery is open
+#actually only 2 specific cases when fishery open but NAs for fuel price
+# season    half_month
+# 2013-2014 December_1
+# 2017-2018 January_1 
+#fill these with the second half of that month (fuel price is monthly)
+#code for this is above
 
 
 #write_rds(study_area_grids_with_all_season_halfmonth_combos_wind_SST_fixed_depth_faults_canyon_escarp_portdist_fuel,here::here('DCRB_sdmTMB', 'data', "study_area_grids_with_all_season_halfmonth_combos_wind_SST_fixed_depth_faults_canyon_escarp_portdist_fuel.rds"))
