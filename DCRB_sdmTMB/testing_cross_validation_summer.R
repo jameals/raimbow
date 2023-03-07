3f4bc330a03956ae846092d92457a5986e0c6dd0
leena-r <leena.riekkola@noaa.gov>
2023-03-08 08:12:34 +1300

    if try eric's fix for summer model get an error

--- DCRB_sdmTMB/testing_cross_validation_summer.R
+++ DCRB_sdmTMB/testing_cross_validation_summer.R
@@ -1572,11 +1572,63 @@ toc()
 
 
 
+#eric's model that apparently works:
+
+tic()
+validation_years <- 2015:2019 # I'd make this no fewer than 5, no more than 10
+cv_fits <- list()
+model_selection <- data.frame(validation_years = validation_years,
+                              elpd = NA,
+                              loglik = NA)
+for(yr in validation_years) {
+  # remove data in future years
+  train <- dplyr::filter(summer, yearn < yr)
+  train$fold_id <- 1
+  test <- dplyr::filter(summer, yearn == yr, month_name == "May")
+  test$fold_id <- 2
+  sub <- rbind(test, train)
+  # make mesh for this dataset
+  mesh <- make_mesh(sub, xy_cols = c("X","Y"), cutoff = 10)
+  # fit model with sdmTMB_cv
+  indx <- yr - min(validation_years) + 1
+  sub$month_name_f <- as.factor(as.character(sub$month_name_f))
+  cv_fits[[indx]] <- sdmTMB_cv(formula = tottraps ~ 0 +
+            season +
+            month_name_f +
+            #OR_WA_waters + #part of interaction term
+            #WA_pot_reduction +
+            z_SST_avg  +
+            z_wind_avg +
+            poly(z_depth_point_mean, 2) +
+            z_depth_point_sd +
+            z_faults_km +
+            z_dist_canyon_km +
+            z_weighted_dist +
+            z_weighted_fuel_pricegal +
+            z_weighted_crab_ppp +
+            z_bottom_O2_avg +
+            OR_WA_waters * z_dist_to_closed_km,
+          family = tweedie(),
+          fold_ids = sub$fold_id,
+          mesh = mesh,
+          spatial = "off",
+          spatiotemporal = "iid",
+          data = sub,
+          control = sdmTMBcontrol(newton_loops = 1),
+          time = "yearn"
+          )
+  #cv_fits[[1]] is now a list of 2 models. We want the second of each of these, 
+  model_selection$elpd[indx] <- cv_fits[[indx]]$fold_elpd[2]
+  model_selection$loglik[indx] <- cv_fits[[indx]]$fold_loglik[2]
+}
+# total the log lik or ELPD now across years
+tot_elpd <- sum(model_selection$elpd)
+tot_loglik <- sum(model_selection$loglik)
+toc()
 
 
-
-
-
+#adding control argument gave this
+#Error in solve.default(h, g) : Lapack routine dgesv: system is exactly singular: U[1,1] = 0
 
 
 
